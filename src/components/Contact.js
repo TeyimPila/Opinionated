14f45bf8d6884dc3eb2cc1ccf217a50baf2f7a2c
aikidoshi <aikidoshi@hotmail.com>
2017-06-12 14:03:57 +1000

    Link to project on Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Contact = () => (
     <div className="well well-sm">
+
         <h2>Contact Page</h2>
-        <p>Steve can be contacted at aikidoshi@hotmail.com</p>
+
+        <p>Steve can be contacted at https://github.com/aikidoshi/Opinionated</p>
+
     </div>
 )
 
